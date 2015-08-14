3ba9b7fe72eaca3678bf3d7dd735a0d35c56de75
jacogr <j@cqscloud.com>
2015-08-14 11:16:42 +0200

    add test (useless) that just runs through defined tasks

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,12 +1,19 @@
 'use strict';
 
 const gulp = require('gulp');
+const serial = require('run-sequence');
 
+const coverMerge = require('./src/gulp/coverage').merge;
 const compileJs = require('./src/gulp/js').compile;
 const lintJs = require('./src/gulp/jslint').lint;
+const testMocha = require('./src/gulp/mocha').test;
 
 const SRC = ['src/**/*.js'];
 const DEST = '.';
+
+gulp.task('coverage', function() {
+  return coverMerge();
+});
 
 gulp.task('js', function() {
   return compileJs(DEST, SRC);
@@ -14,6 +21,14 @@ gulp.task('js', function() {
 
 gulp.task('lint', function() {
   return lintJs(SRC);
+});
+
+gulp.task('test-server', function(cb) {
+  return testMocha(cb, ['src/**/*.js'], ['spec/server/*.spec.js']);
+});
+
+gulp.task('test', function(cb) {
+  return serial('test-server', 'coverage', cb);
 });
 
 gulp.task('default', ['lint', 'js']);
---
+++ spec/server/index.spec.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('useless test', function() {
+  it('should do absolutely nothing', function() {
+    expect(true).to.be.true;
+  });
+});
