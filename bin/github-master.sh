9a6739ea3be884d176fa3c00166d0de51a43cf72
jacogr <j@cqscloud.com>
2015-11-21 12:21:07 +0200

    add github master package push

--- bin/github-master.sh
+++ bin/github-master.sh
@@ -10,6 +10,10 @@ git config --global user.name "BuildBot"
 git config branch.master.remote origin
 git config branch.master.merge refs/heads/master
 
+# setup files needed on master
+sed -i -e 's/dist\///' .gitignore
+rm -rf spec src .babelrc .codeclimate.yml .eslintrc .jshintrc .node-version bower.json circle.yml gulpfile.js karma.conf.js
+
 # package version
 sed -i -e "s/\"version\": \"1.0.0\"/\"version\": \"$VERSION\"/" package.json
 
