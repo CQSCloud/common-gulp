69bbb5d2d5db8d6d391fd8a44d68c79d7e75c255
jacogr <j@cqscloud.com>
2016-04-10 14:32:11 +0200

    fix mixed sass & scss files

--- spec/compile/sass.spec.js
+++ spec/compile/sass.spec.js
@@ -2,7 +2,7 @@ const expect = require('chai').expect;
 const fs = require('fs');
 
 describe('sass output', function() {
-  const CSS_EXPECTED = 'body{background:#fff;color:red}'; // eslint-disable-line
+  const CSS_EXPECTED = 'body{background:#fff;color:red}\nmeta.foundation-version{font-family:"/5.4.7/"}'; // eslint-disable-line
 
   it('compiles css', function() {
     expect(fs.readFileSync('dist/test.css').toString()).to.equal(CSS_EXPECTED);
