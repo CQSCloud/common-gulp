1f1b38b658e6aaa068569628d2ae9d65ff2ab775
Matthew Davies <matthewdavies@lunasoft.co.za>
2022-01-07 12:56:41 +0200

    Rename jslint for series

--- tasks/base/jslint.js
+++ tasks/base/jslint.js
@@ -3,11 +3,9 @@
 const gulp = require('gulp');
 const eslint = require('gulp-eslint');
 
-const task =  (src, opt) => {
-  return gulp
-    .src(src, { allowEmpty: true })
-    .pipe(eslint(opt || {}))
-    .pipe(eslint.format());
-};
+const jsLint = (src, opt) => gulp
+  .src(src, { allowEmpty: true })
+  .pipe(eslint(opt || {}))
+  .pipe(eslint.format());
 
-module.exports = task;
+module.exports = jsLint;
