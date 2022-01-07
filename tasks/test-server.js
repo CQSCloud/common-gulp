078d1bb080b577c934751fa3905899ec079af229
Matthew Davies <matthewdavies@lunasoft.co.za>
2022-01-07 11:24:24 +0200

    Fix test test-server to use requires instead of strings in series

--- tasks/test-server.js
+++ tasks/test-server.js
@@ -1,9 +1,9 @@
 ﻿const gulp = require('gulp');
 const mocha = require('./base/mocha');
-
+const testServerPrereq = require('./test-server-prereq');
 const task = () => {
-  return gulp.series('test-server-prereq', (done) => {
-    return mocha(done, ['src/server/scripts/**/*.js', 'src/shared/scripts/**/*.js'], [
+  return gulp.series(testServerPrereq, (cb) => {
+    return mocha(cb, ['src/server/scripts/**/*.js', 'src/shared/scripts/**/*.js'], [
       'spec/server/helpers/index.js', 'spec/shared/**/*.spec.js', 'spec/server/**/*.spec.js'
     ]);
   });
--- tasks/test.js
+++ tasks/test.js
@@ -1,7 +1,10 @@
 ﻿const gulp = require('gulp');
+const testServer = require('./test-server');
+const testClient = require('./test-client');
+const coverage = require('./base/coverage');
 
 const task = () => {
-  return gulp.series('test-server', 'test-client', 'coverage');
+  return gulp.series(testServer, testClient, coverage);
 };
 
 module.exports = task;
