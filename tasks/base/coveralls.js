de61edc241ff871ec6daa68809bc52f3695657fa
Michael Wiles <mike@lunasoft.co.za>
2022-01-07 10:42:26 +0200

    -> Fixed base coveralls exec reference

--- tasks/base/coveralls.js
+++ tasks/base/coveralls.js
@@ -1,13 +1,13 @@
 'use strict';
 
 const path = require('path');
-const exec = require('./exec-wrapper');
+const exec = require('../exec/exec-wrapper');
 
-const task = (cb) => {
+const task = (done) => {
   const lcov = path.join(process.cwd(), 'coverage/lcov.info');
   const bin = path.join(process.cwd(), 'node_modules/.bin/coveralls');
   const cmd = [bin, '<', lcov].join(' ');
-  exec(cmd, cb);
+  exec(cmd, done);
 };
 
 module.exports = task;
