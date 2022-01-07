25734df430e39da7ac1a0676bfd045c6784bbe7a
Matthew Davies <matthewdavies@lunasoft.co.za>
2022-01-07 12:57:12 +0200

    rename sass for series

--- tasks/base/transpile-sass.js
+++ tasks/base/transpile-sass.js
@@ -7,16 +7,18 @@ const gulpif = require('gulp-if');
 const ignore = require('gulp-ignore');
 const sass = require('gulp-sass')(require('sass'));
 
-const errcb = require('../lib/error-callback');
+const errorCallback = require('../lib/error-callback');
 const { isProduction } = require('../lib/is-env');
 
-const task = (src, includePaths, _options) => {
+const transpileSass = (src, includePaths, _options) => {
   const options = _options || {};
   const destDir = options.destDir || 'dist/public/styles';
   const destFile = options.destFile || 'client.css';
   const ignorePaths = options.ignore || ['**/src/client/styles/application.sass'];
 
-  const cleanedInput = gulp.src(src, { allowEmpty: true }).pipe(ignore.include(true, ignorePaths));
+  const cleanedInput = gulp
+    .src(src, { allowEmpty: true })
+    .pipe(ignore.include(true, ignorePaths));
 
   return cleanedInput
     .pipe(sass({
@@ -24,7 +26,7 @@ const task = (src, includePaths, _options) => {
       outputStyle: 'expanded',
       includePaths: includePaths || []
     }))
-    .on('error', errcb)
+    .on('error', errorCallback)
     .pipe(ignore.exclude('*.css.map'))
     .pipe(gulpif(isProduction(), cssmin({
       keepBreaks: true,
@@ -34,4 +36,4 @@ const task = (src, includePaths, _options) => {
     .pipe(gulp.dest(destDir));
 };
 
-module.exports = task;
+module.exports = transpileSass;
