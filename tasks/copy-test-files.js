4566392b546045dd2f847fc4377a7daf9b1d4540
Matthew Davies <matthewdavies@lunasoft.co.za>
2022-01-07 10:58:14 +0200

    Replace promise chain with gulp series

--- tasks/copy-test-files.js
+++ tasks/copy-test-files.js
@@ -1,31 +1,22 @@
 ﻿const copy =  require('./base/copy');
 const gulp = require('gulp');
+const ts = require('gulp-typescript');
+const tsProject = ts.createProject('./tsconfig.json');
 
-const task = () => {
-  gulp.src('src/server/scripts/**/*.ts', {
-    base: '.'
-  }, {
-    allowEmpty: true
-  })
-    .pipe(tsProject())
-    .js
-    .pipe(gulp.dest('test'));
-
-  return new Promise(resolver => {
-    copy('test/src/shared', 'src/shared/**/*.*')
-      .on('finish', () => {
-        copy('test/src/cwIntegration', 'src/cwIntegration/**/*.*')
-          .on('finish', () => {
-            copy('test/src/database', 'src/database/**/*.*')
-              .on('finish', () => {
-                copy('test/src/server', 'src/server/**/*.*')
-                  .on('finish', () => {
-                    resolver();
-                  });
-              });
-          });
-      });
-  });
-};
+const task = () =>
+  gulp.series(
+    gulp.src('src/server/scripts/**/*.ts', {
+      base: '.'
+    }, {
+      allowEmpty: true
+    })
+      .pipe(tsProject())
+      .js
+      .pipe(gulp.dest('test')),
+    copy('test/src/shared', 'src/shared/**/*.*'),
+    copy('test/src/cwIntegration', 'src/cwIntegration/**/*.*'),
+    copy('test/src/database', 'src/database/**/*.*'),
+    copy('test/src/server', 'src/server/**/*.*')
+  );
 
 module.exports = task;
