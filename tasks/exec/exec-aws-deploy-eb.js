a668c8cf89a8d631f7cb98de08b5f7294ff0a382
Matthew Davies <matthewdavies@lunasoft.co.za>
2022-01-18 09:46:21 +0200

    Fix paths for exec scripts

--- tasks/exec/exec-aws-deploy-eb.js
+++ tasks/exec/exec-aws-deploy-eb.js
@@ -5,7 +5,7 @@ const argv = require('yargs').argv;
 const exec = require('./exec-wrapper');
 
 const task = (cb) => {
-  const bin = path.join(__dirname, '../bin/aws-deploy-eb.sh');
+  const bin = path.join(__dirname, '../../bin/aws-deploy-eb.sh');
   const cmd = ['bash', bin, argv.env, argv.cname].join(' ');
   exec(cmd, cb);
 };
