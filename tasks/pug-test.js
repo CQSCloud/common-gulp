df1fb1ea63352013916c823f69df092f03566428
Nico von Ronge <nicov@cqs.co.za>
2022-02-16 14:16:40 +0200

    renamed jade files to pug

--- tasks/pug-test.js
+++ tasks/pug-test.js
@@ -1,6 +1,6 @@
 const pug = require('./base/pug');
 
-const task = () => pug('dist/', ['spec/compile/test/*.jade']);
+const task = () => pug('dist/', ['spec/compile/test/*.jade', 'spec/compile/test/*.pug']);
 
 task.displayName = 'pug';
 task.description = 'Compile Test Html from Pug Templates';
